538ca046833cb46b9b3ea50567385dc91916a7b2
Rohan Singh <1andonly1rohan@gmail.com>
2020-08-25 23:13:16 +0530

    2

--- src/constants/About.js
+++ src/constants/About.js
@@ -1,5 +1,5 @@
 export const about = {
     heading: "About Me",
-    quote: "A language that doesn't affect the way you think \nabout programming is not worth knowing",
-    bio: "I am a computer science undergrad student at Chandigarh University with current CGPA of 7.12.\nI have completed my 10+2 from Kendriya Vidyalaya  with 75%.\nOther than technical skills my interest is in listening to music and playing online multiplayer games."
+    quote: "A language that doesn't affect the way you think about programming is not worth knowing",
+    bio: "I am a computer science undergraduate from  Chandigarh University with current CGPA of 7.12.\nI have completed my 10+2 from Kendriya Vidyalaya  with 75%.\nOther than technical skills, my interests are listening to music and playing online multiplayer games."
 };
--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -4,7 +4,7 @@ export const projects = {
     projectList: [
         {
             name: "Product Hunt",
-            description: "Product hunt is a web application.In this web application user can upload their ideas, startup or new website they have made.And other users on that website can see their post and can up vote and down vote their post. The backend is based on django and frontend is made from bootstrap.",
+            description: "Product hunt is a web application.In this web application users can upload their ideaa, startups or new websites they have made.Other users on the website can see their posts and can up vote and down vote their posts. The backend is based on django and frontend is made from bootstrap.",
             link: "https://huntyourproduct.herokuapp.com/"
         }, {
             name: "Monster Rolex",
